e23394b0fdf88e46f740d195b00072330f9fd75d
Antonio Barbosa <acrb1108@gmail.com>
2023-12-03 16:02:55 -0300

    Inserindo a entidade Cliente.

---
+++ ProjetoAula04/Entities/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula04.Entities
+{
+    /// <summary>
+    /// Classe de entidade
+    /// </summary>
+    public class Cliente
+    {
+        public Guid IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
