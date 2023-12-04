d3add727ef778fb2f2b4146a581ee4457a2c5f57
Antonio Barbosa <acrb1108@gmail.com>
2023-12-04 13:31:21 -0300

    Criando a ClienteRepository

---
+++ ProjetoAula04/Repositories/ClienteRepository.cs
@@ -0,0 +1,46 @@
+﻿using ProjetoAula04.Entities;
+using ProjetoAula04.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula04.Repositories
+{
+    /// <summary>
+    /// Classe para implementar o repositorio de cliente
+    /// </summary>
+    public class ClienteRepository : IClienteRepository
+    {
+        public void Create(Cliente obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Cliente obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Cliente obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> GetByNome(string nome)
+        {
+            throw new NotImplementedException();
+        }       
+    }
+}
