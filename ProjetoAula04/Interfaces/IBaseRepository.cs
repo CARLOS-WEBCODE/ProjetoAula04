401d81f35b3e284b4d770efdadc296bc81d9cc37
Antonio Barbosa <acrb1108@gmail.com>
2023-12-04 13:18:45 -0300

    Criando a interface base (CRUD)

---
+++ ProjetoAula04/Interfaces/IBaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula04.Interfaces
+{
+    /// <summary>
+    /// Interface ge´nérica para definição dos métodos do repositório
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IBaseRepository<T>
+        where T : class
+    {
+        void Create(T obj);
+        void Update(T obj);
+        void Delete(T obj);
+
+        List<T> GetAll();
+        T GetById(Guid id);
+    }
+}
